438ccec49a3e918eba919e174e2d263de32bee13
Gabriel Brickle <gabebrickle@Gabriels-MacBook-Pro.local>
2016-05-31 16:45:29 -0400

    done

--- popup.js
+++ popup.js
@@ -13,7 +13,16 @@ function pagePics(links) {
   var sitePhotos = document.querySelectorAll('img');
   sitePhotos = Array.from(sitePhotos)
   sitePhotos.forEach(function (element,idx,arr){
-    element.photo= "http://ecx.images-amazon.com/images/I/71Qwx0Q4eaL._SL1000_.jpg";
+    element.src ="http://ecx.images-amazon.com/images/I/71Qwx0Q4eaL._SL1000_.jpg";
   })
 }
 pagePics()
+
+function pageParagraph(links) {
+  var siteParagraphs = document.querySelectorAll('p');
+  siteParagraphs = Array.from(siteParagraphs)
+  siteParagraphs.forEach(function (element,idx,arr){
+    element.innerText ="DANCE";
+  })
+}
+pageParagraph()
